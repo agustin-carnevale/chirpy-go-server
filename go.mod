a6ed1bdcd4ae53a7443a4e43f78067a19cdeab0b
Agustin Carnevale <agustinc.dev@gmail.com>
2025-03-06 05:49:33 -0300

    first file server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/agustin-carnevale/chirpy-go-server
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Uppps something went wrong. Server did not start.")
+	}
+
+}
