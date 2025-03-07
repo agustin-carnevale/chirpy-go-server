a42222037be4382242b249e70b50d1f890ef4629
Agustin Carnevale <agustinc.dev@gmail.com>
2025-03-06 21:14:17 -0300

    middlewares, handlers and http methods

--- main.go
+++ main.go
@@ -3,7 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func readinessHandler(w http.ResponseWriter, req *http.Request) {
 	// set Header
@@ -14,13 +26,48 @@ func readinessHandler(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
+func (cfg *apiConfig) hitsCountHandler(w http.ResponseWriter, req *http.Request) {
+	// set Header
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+
+	//Status code
+	w.WriteHeader(http.StatusOK)
+
+	// Write the response body
+	text := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	w.Write([]byte(text))
+}
+
+func (cfg *apiConfig) hitsResetHandler(w http.ResponseWriter, req *http.Request) {
+	// reset fileserverHits to 0
+	cfg.fileserverHits.Store(0)
+
+	// set Header
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+
+	//Status code
+	w.WriteHeader(http.StatusOK)
+
+	// Write the response body
+	text := "Hits: 0"
+	w.Write([]byte(text))
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
+	// Route Handlers
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", readinessHandler)
+	// File Server
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+
+	mux.HandleFunc("GET /healthz", readinessHandler)
+	mux.HandleFunc("GET /metrics", apiCfg.hitsCountHandler)
+	mux.HandleFunc("POST /reset", apiCfg.hitsResetHandler)
 
 	server := http.Server{
 		Addr:    ":" + port,
@@ -33,3 +80,10 @@ func main() {
 	}
 
 }
+
+// func middlewareLog(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		log.Printf("%s %s", r.Method, r.URL.Path)
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
